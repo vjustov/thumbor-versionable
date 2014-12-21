7fcf845308cd88336da0e38916bd48d2b2fa99eb
Viktor Justo Vasquez <viktor@vjustov.me>
2014-12-21 11:25:51 -0400

    Adds Travis, Code Climate and Hound.
    TravisCI for running the tests.
    Hound for checking the Styleguide compliance
    And And Code Climate for code quality.
    Adds all the badges to the readme.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,12 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 # Dir[File.'../lib/**/*.rb'].each { |file| require file }
 require 'versionable'
 require 'versionable/image'
 require 'versionable/version'
 require 'versionable/configuration'
+
 RSpec.configure do |config|
   config.color = true
   config.formatter = :documentation # :progress, :html, :textmate
